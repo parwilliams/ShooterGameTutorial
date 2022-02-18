f00d4cd7c3d521176ab52f8bc71f2782f7706a27
ConnorLWilliams <connor.lee.williams7@gmail.com>
2022-02-17 21:03:15 -0600

    Consistent Shooting
    Shooting at a consistent speed is now possible

--- ShooterGameTutorial-Unity/Assets/Scripts/PlayerController.cs
+++ ShooterGameTutorial-Unity/Assets/Scripts/PlayerController.cs
@@ -10,39 +10,43 @@ public class PlayerController : MonoBehaviour
     public string FireAxis = "Fire1";
     public float ReloadDelay = 0.3f;
     public bool CanFire = true;
-    public Transform[] TurretTransforms;
+    //public Transform[] TurretTransforms;
     public float MaxSpeed = 5f;
     private Rigidbody ThisBody = null;
+
+    //Used for revamp of firing, 
+    public Transform firePoint;
+    public GameObject ammoPrefab;
 
 
     void Awake(){
         ThisBody = GetComponent<Rigidbody>();
     }
 
-    void FixedUpdate(){
+    void FixedUpdate() {
         float Horz = Input.GetAxis(HorzAxis);
         float Vert = Input.GetAxis(VertAxis);
-       
+
         Vector3 MoveDirection = new Vector3(Horz, 0.0f, Vert);
 
         ThisBody.AddForce(MoveDirection.normalized * MaxSpeed);
 
-        ThisBody.velocity = new Vector3(Mathf.Clamp(ThisBody.velocity.x, - MaxSpeed, MaxSpeed), 
-                                       Mathf.Clamp(ThisBody.velocity.y, - MaxSpeed, MaxSpeed), 
+        ThisBody.velocity = new Vector3(Mathf.Clamp(ThisBody.velocity.x, -MaxSpeed, MaxSpeed),
+                                       Mathf.Clamp(ThisBody.velocity.y, -MaxSpeed, MaxSpeed),
                                        Mathf.Clamp(ThisBody.velocity.z, -MaxSpeed, MaxSpeed));
 
-        if(MouseLook){
+        if (MouseLook) {
             Vector3 MousePosWorld = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0.0f));
-            
-            MousePosWorld = new Vector3 (MousePosWorld.x, 0.0f, MousePosWorld.z);
+
+            MousePosWorld = new Vector3(MousePosWorld.x, 0.0f, MousePosWorld.z);
 
             Vector3 LookDirection = MousePosWorld - transform.position;
 
             transform.localRotation = Quaternion.LookRotation(LookDirection.normalized, Vector3.up);
 
-            
-        }
 
+        }
+        /* This did not work for firing ammo prefabs (not sure why), trying a different approach
         if (Input.GetButtonDown(FireAxis) && CanFire)
         {
             foreach (Transform T in TurretTransforms)
@@ -59,5 +63,23 @@ public class PlayerController : MonoBehaviour
     {
         CanFire = true;
     }
+        */
+    }
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1") && CanFire){
+            CanFire = false;
+            Invoke("Shoot", ReloadDelay);
+        }
+    }
+      
+    void Shoot()
+    {
+        //Shooting logic
+        Instantiate(ammoPrefab, firePoint.position, firePoint.rotation);
+        CanFire = true;
+    }
+
 
 }
